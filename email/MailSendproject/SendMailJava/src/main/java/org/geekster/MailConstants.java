0b584795e02717e74c5b3fae5dfd6aab702b6e6f
md sanowar alam <94783739+Sanowar72@users.noreply.github.com>
2023-04-15 15:46:44 +0530

    first commit

---
+++ email/MailSendproject/SendMailJava/src/main/java/org/geekster/MailConstants.java
@@ -0,0 +1,15 @@
+package org.geekster;
+
+public class MailConstants {
+
+    public static final String SENDER = "mdsanowaralam79@gmail.com";
+
+    // receiver email
+    public static final String RECEIVERMAILADDRESS = "atifreza72ar@gmail.com";
+
+    // subject of the mail
+    public static final String SUBJECT = "Testing Java mail : im able to send email or not";
+
+    // message to be sent in the mail body
+    public static final String MESSAGE = "This message is for security check...!!!";
+}
